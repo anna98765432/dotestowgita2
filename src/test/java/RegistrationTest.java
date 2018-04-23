78d599ede544c9bf4c8f44bdd2a0516be7a57586
Michalik12345678 <anna.michalik789@gmail.com>
2018-04-23 18:17:00 +0200

    dodanie magicznej metody

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,6 +11,39 @@ import java.util.UUID;
 
 
 public class RegistrationTest {
+
+    public boolean waitForJStoLoad() {
+
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        try {
+            Thread.sleep(100);  //sleep 100ms to be sure there is no delay in browser befor kicking of jquery or js
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // wait for jQuery to load
+        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                try {
+                    return ((Long)((JavascriptExecutor)driver).executeScript("return jQuery.active") == 0);
+                }
+                catch (Exception e) {
+                    return true;
+                }
+            }
+        };
+
+        // wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor)driver).executeScript("return document.readyState")
+                        .toString().equals("complete");
+            }
+        };
+        return wait.until(jQueryLoad) && wait.until(jsLoad);
+    }
+
+
 
     public void signIn(String name, String name2, String street, String city, String state, String postCode,
                        String customerNumer){
